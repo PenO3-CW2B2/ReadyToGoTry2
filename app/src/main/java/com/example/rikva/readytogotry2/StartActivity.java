c9994a68f8c09255ca733b549c9e2a764087e6bb
Kristof Van Loock <kristof.vanloock@skynet.be>
2018-10-21 20:45:52 +0200

    Edited design StartActivity

--- app/src/main/java/com/example/rikva/readytogotry2/StartActivity.java
+++ app/src/main/java/com/example/rikva/readytogotry2/StartActivity.java
@@ -1,7 +1,10 @@
 package com.example.rikva.readytogotry2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,4 +13,9 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    public void switchToLoginActivity(View view) {
+        startActivity(new Intent(StartActivity.this, LoginActivity.class));
+    }
+
 }

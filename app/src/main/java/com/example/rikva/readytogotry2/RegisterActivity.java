5abc0d9b1fee2c031164e5ccefcf73c8fc2a93be
Kristof Van Loock <kristof.vanloock@skynet.be>
2018-10-25 18:41:57 +0200

    edit register activity

--- app/src/main/java/com/example/rikva/readytogotry2/RegisterActivity.java
+++ app/src/main/java/com/example/rikva/readytogotry2/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.rikva.readytogotry2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override

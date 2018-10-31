8f33988f02f03b3899014b99de0c69e380d90e63
Kristof Van Loock <kristof.vanloock@skynet.be>
2018-10-31 10:31:09 +0100

    Added link to HomeActivity

--- app/src/main/java/com/example/rikva/readytogotry2/SignInActivity.java
+++ app/src/main/java/com/example/rikva/readytogotry2/SignInActivity.java
@@ -1,15 +1,14 @@
 package com.example.rikva.readytogotry2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -36,7 +35,7 @@ public class SignInActivity extends AppCompatActivity {
 
         Context context = getApplicationContext();
         CharSequence text_success = "Successful Signed In";
-        CharSequence text_fail = "Unsuccessfully Signed In";
+        CharSequence text_fail = "Wrong username or password";
         int duration = Toast.LENGTH_SHORT;
 
         final Toast toast_success = Toast.makeText(context, text_success, duration);
@@ -49,8 +48,8 @@ public class SignInActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess() {
                         toast_success.show();
+                        startActivity(new Intent(SignInActivity.this, HomeActivity.class));
                     }
-
                     @Override
                     public void onFailure() {
                         toast_fail.show();
@@ -117,5 +116,4 @@ public class SignInActivity extends AppCompatActivity {
         void onSuccess();
         void onFailure();
     }
-
 }

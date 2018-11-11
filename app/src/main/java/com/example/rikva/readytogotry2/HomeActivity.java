f0141a40f539d1d0e2acd2018121d77102c9e49c
Kristof Van Loock <kristof.vanloock@skynet.be>
2018-11-11 12:58:12 +0100

    Fixed location permission issue

--- app/src/main/java/com/example/rikva/readytogotry2/HomeActivity.java
+++ app/src/main/java/com/example/rikva/readytogotry2/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.rikva.readytogotry2;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
--- app/src/main/java/com/example/rikva/readytogotry2/MapActivity.java
+++ app/src/main/java/com/example/rikva/readytogotry2/MapActivity.java
@@ -50,6 +50,12 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    10);
+        }
+
         //startLocationUpdates();
 
 
@@ -74,12 +80,7 @@ public class MapActivity extends AppCompatActivity {
         IMapController mapController = map.getController();
         mapController.setZoom(15.0);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    10);
-            return;
-        }
+
 
         this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx),map);
         this.mLocationOverlay.enableMyLocation();

86924b2f8cd4f18ad854458d2e069eb5f209ab00
Rik <rik.vaerenberg@hotmail.com>
2018-11-30 16:03:05 +0100

    Minor fixes

--- app/src/main/java/com/example/rikva/readytogotry2/StartActivity.java
+++ app/src/main/java/com/example/rikva/readytogotry2/StartActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
